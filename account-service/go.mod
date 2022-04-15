b5b15b32601e7ea2b25d210564f79bcd1624af40
Nikolas North <nikolasn09@gmail.com>
2022-04-14 21:45:31 -0400

    setup connection to postgres db

--- account-service/go.mod
+++ account-service/go.mod
@@ -2,4 +2,7 @@ module account-service
 
 go 1.18
 
-require github.com/go-chi/chi/v5 v5.0.7 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.0.7 // indirect
+	github.com/lib/pq v1.10.5 // indirect
+)
--- account-service/main.go
+++ account-service/main.go
@@ -1,14 +1,57 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
+	"os"
+)
+
+type Config struct {
+	DbHost     string `json:"dbHost"`
+	DbPort     int    `json:"dbPort"`
+	DbUser     string `json:"dbUser"`
+	DbPassword string `json:"dbPassword"`
+	DbName     string `json:"dbName"`
+}
+
+const (
+	configFilename = "config.json"
+	port           = ":8000"
 )
 
 func main() {
+	contents, err := os.ReadFile(configFilename)
+	if err != nil {
+		log.Fatalf("failed to open %s: %v", configFilename, err)
+	}
+
+	config := Config{}
+	err = json.Unmarshal(contents, &config)
+	if err != nil {
+		log.Fatalf("failed to unmarshal config file: %v", err)
+	}
+
 	r := chi.NewRouter()
+
+	db, err := sql.Open("postgres", fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s",
+		config.DbHost, config.DbPort, config.DbUser, config.DbPassword, config.DbName,
+	))
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("failed to ping database: %v", err)
+	}
+
 	r.Use(middleware.Logger)
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
@@ -18,7 +61,8 @@ func main() {
 		}
 	})
 
-	err := http.ListenAndServe(":8000", r)
+	log.Println("listening on http://localhost" + port)
+	err = http.ListenAndServe(port, r)
 	if err != nil {
 		log.Fatalf("failed to start server: %v", err)
 	}
